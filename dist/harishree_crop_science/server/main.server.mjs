a70bcd92de01426de64ec69af37300b78aab88f2
aalekhanSoftware <aalekhansoftware@gmail.com>
2026-07-13 20:36:56 +0530

    feat: Remove Lanser Gold product references and related components
    - Deleted the Lanser Gold (Acephate 50% + Imidacloprid 1.8% SP) product component, including its HTML, SCSS, and TypeScript files.
    - Updated routing to remove references to Lanser Gold, ensuring no broken links in the application.
    - Cleaned up related products sections across multiple components to exclude Lanser Gold.
    - Enhanced sitemap by removing outdated URLs associated with Lanser Gold for improved SEO.

---
+++ dist/harishree_crop_science/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WFG63HXD.mjs";import"./chunk-MNTE7ZU2.mjs";import"./chunk-JEVLDQST.mjs";import"./chunk-RGDALSI3.mjs";import"./chunk-UCHOAAGB.mjs";import"./chunk-EVADDDIG.mjs";import"./chunk-63IAZCJ2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
